65d59de5ec9b871eaf391e73c4cde3f7ae72edcc
rhruska <robin.hruska@teskalabs.com>
2022-02-01 15:34:05 +0100

    Session: Supply instructions for setting AES key

--- seacatauth/session/service.py
+++ seacatauth/session/service.py
@@ -32,14 +32,13 @@ class SessionService(asab.Service):
 
 		aes_key = asab.Config.get("seacatauth:session", "aes_key")
 		if len(aes_key) == 0:
-			raise ValueError("""
-				Session AES key must not be empty. 
+			raise ValueError("""Session AES key must not be empty. 
 				Please specify it in the [seacatauth:session] section of your Seacat Auth configuration file.
 				You may use the following randomly generated example:
 				
 				[seacatauth:session]
 				aes_key={}
-			""".replace("\t", "").format(secrets.token_urlsafe(24)))
+			""".replace("\t", "").format(secrets.token_urlsafe(16)))
 		self.AESKey = hashlib.sha256(aes_key.encode("utf-8")).digest()
 		# Block size is used for determining the size of CBC initialization vector
 		self.AESBlockSize = cryptography.hazmat.primitives.ciphers.algorithms.AES.block_size // 8
