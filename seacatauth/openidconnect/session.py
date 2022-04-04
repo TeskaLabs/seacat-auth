af7c979766c4edbfa4a1c71064517c45cda747f2
Robin Hruška <byewokko@seznam.cz>
2022-04-04 10:51:09 +0200

    OIDC builder: Save scope as list

--- seacatauth/openidconnect/session.py
+++ seacatauth/openidconnect/session.py
@@ -7,7 +7,12 @@ def oauth2_session_builder(oauth2_data):
 	# Token length must be a multiple of AES block size (= 16 bytes)
 	token_length = 16 + 32  # The first part is AES CBC init vector, the second is the actual token
 
-	yield (SessionAdapter.FNOAuth2Scope, " ".join(oauth2_data["scope"]))
+	# TODO: Scope should be always present
+	scope = oauth2_data.get("scope")
+	if scope is not None:
+		scope = list(scope)
+
+	yield (SessionAdapter.FNOAuth2Scope, scope)
 	yield (SessionAdapter.FNOAuth2ClientId, oauth2_data["client_id"])
 	yield (SessionAdapter.FNOAuth2AccessToken, secrets.token_bytes(token_length))
 	yield (SessionAdapter.FNOAuth2RefreshToken, secrets.token_bytes(token_length))
