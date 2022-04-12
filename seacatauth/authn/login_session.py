fdcfa71efbecf909040f74ace2129ac6184d0737
Robin Hruška <byewokko@seznam.cz>
2022-04-12 16:58:10 +0200

    Webauthn: Fix json response

--- seacatauth/authn/login_session.py
+++ seacatauth/authn/login_session.py
@@ -1,6 +1,7 @@
 import json
 import secrets
 import datetime
+import typing
 
 import cryptography.hazmat.backends
 import cryptography.hazmat.primitives.asymmetric.ec
@@ -60,10 +61,12 @@ class LoginSession(object):
 		return json.loads(decryptor.update(message) + decryptor.finalize())
 
 
-	def encrypt(self, plaintext: dict) -> bytes:
+	def encrypt(self, plaintext: typing.Union[str, dict, bytes]) -> bytes:
 		assert self.__shared_key is not None
 		if isinstance(plaintext, dict):
 			plaintext = json.dumps(plaintext).encode('utf-8')
+		elif isinstance(plaintext, str):
+			plaintext = plaintext.encode('utf-8')
 
 		iv = secrets.token_bytes(12)
 
