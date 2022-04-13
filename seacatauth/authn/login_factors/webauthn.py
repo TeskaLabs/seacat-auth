3daacaa873b150075aecdb2114991aba698f46d6
Robin Hruška <byewokko@seznam.cz>
2022-04-13 14:21:33 +0200

    WebAuthn: Update login factor

--- seacatauth/authn/login_factors/webauthn.py
+++ seacatauth/authn/login_factors/webauthn.py
@@ -13,14 +13,16 @@ class WebAuthnFactor(LoginFactorABC):
 	Type = "webauthn"
 
 	async def is_eligible(self, login_data) -> bool:
-		cred_svc = self.AuthenticationService.CredentialsService
-		cred_id = login_data["credentials_id"]
-		if cred_id == "":
+		"""
+		User is eligible for WebAuthn login if they have at least one WebAuthn credential registered
+		"""
+		credentials_id = login_data["credentials_id"]
+		if credentials_id == "":
 			# Not eligible for "fake" login session
 			return False
-		credentials = await cred_svc.get(cred_id, include=frozenset(["__webauthn"]))
-		public_key = credentials.get("__webauthn", "")
-		if len(public_key) > 0:
+		webauthn_svc = self.AuthenticationService.App.get_service("seacatauth.WebAuthnService")
+		webauthn_credentials = await webauthn_svc.get_webauthn_credentials_by_user(credentials_id)
+		if len(webauthn_credentials) > 0:
 			return True
 		return False
 
