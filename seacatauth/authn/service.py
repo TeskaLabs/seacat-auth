6d435ce6f36944e224bf03ab452603095e1ed243
Robin Hruška <byewokko@seznam.cz>
2022-04-13 14:15:10 +0200

    Login: Add webauthn to default login descriptors

--- seacatauth/authn/service.py
+++ seacatauth/authn/service.py
@@ -26,22 +26,26 @@ LOGIN_DESCRIPTOR_FALLBACK = [
 		'id': 'default',
 		'label': 'Use default login',
 		'factors': [
+			# If TOTP is active, two-factor login should be required by default
 			[
-				# If TOTP is active, it should be required by default as a second factor
-				{'id': 'password', 'type': 'password'},
-				{'id': 'webauthn', 'type': 'webauthn'}
-			],
-			[
-				# If TOTP is active, it should be required by default as a second factor
 				{'id': 'password', 'type': 'password'},
 				{'id': 'totp', 'type': 'totp'}
 			],
 			[
-				# Otherwise, require only password
 				{'id': 'password', 'type': 'password'}
 			]
 		],
-	}
+	},
+	{
+		'id': 'webauthn',
+		'label': 'WebAuthn login',
+		'factors': [
+			[
+				{'id': 'password', 'type': 'password'},
+				{'id': 'webauthn', 'type': 'webauthn'}
+			],
+		],
+	},
 ]
 
 
