bdca22d62f0f714c9f1f68307c2495e6ac0cc7e0
Robin Hruška <byewokko@seznam.cz>
2022-04-22 09:43:13 +0200

    TOTP: Store secrets in database

--- seacatauth/otp/handler.py
+++ seacatauth/otp/handler.py
@@ -20,22 +20,22 @@ class OTPHandler(object):
 		web_app_public = app.PublicWebContainer.WebApp
 
 		web_app = app.WebContainer.WebApp
-		web_app.router.add_get('/public/totp', self.get_totp)
+		web_app.router.add_get('/public/totp', self.get_totp_secret)
 		web_app.router.add_put('/public/set-totp', self.set_totp)
 		web_app.router.add_put('/public/unset-totp', self.unset_totp)
 
 		# Public endpoints
-		web_app_public.router.add_get('/public/totp', self.get_totp)
+		web_app_public.router.add_get('/public/totp', self.get_totp_secret)
 		web_app_public.router.add_put('/public/set-totp', self.set_totp)
 		web_app_public.router.add_put('/public/unset-totp', self.unset_totp)
 
 	@access_control()
-	async def get_totp(self, request, *, credentials_id):
+	async def get_totp_secret(self, request, *, credentials_id):
 		"""
 		Returns the status of TOTP setting.
 		If not activated, it also generates and returns a new TOTP secret.
 		"""
-		response = await self.OTPService.get_totp(request.Session, credentials_id)
+		response = await self.OTPService.get_totp_secret(request.Session, credentials_id)
 		return asab.web.rest.json_response(request, response)
 
 	@asab.web.rest.json_schema_handler({
