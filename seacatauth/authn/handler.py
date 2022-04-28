9611acfa1b80d1a733c1b775bd9e836ac9dd7c7d
Robin Hruška <byewokko@seznam.cz>
2022-04-28 12:02:59 +0200

    Time: Use datetime.datetime.utcnow

--- seacatauth/authn/handler.py
+++ seacatauth/authn/handler.py
@@ -274,7 +274,7 @@ class AuthenticationHandler(object):
 			return aiohttp.web.Response(body=login_session.encrypt(body))
 
 		# Webauthn challenge timeout should be the same as the current login session timeout
-		timeout = (login_session.ExpiresAt - datetime.datetime.now()).total_seconds() * 1000
+		timeout = (login_session.ExpiresAt - datetime.datetime.utcnow()).total_seconds() * 1000
 
 		webauthn_svc = self.AuthenticationService.App.get_service("seacatauth.WebAuthnService")
 		authentication_options = await webauthn_svc.get_authentication_options(
