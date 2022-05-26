45542d1676e55263b78064af9a44a9d9fe2c9076
Robin Hruška <byewokko@seznam.cz>
2022-05-26 16:37:45 +0200

    Credentials: MySQL provider skeleton

---
+++ seacatauth/credentials/providers/mysql.py
@@ -0,0 +1,129 @@
+import logging
+from typing import Optional
+
+import asab
+import aiomysql
+
+from .abc import EditableCredentialsProviderABC
+
+#
+
+L = logging.getLogger(__name__)
+
+#
+
+
+class MySQLCredentialsService(asab.Service):
+
+	def __init__(self, app, service_name="seacatauth.credentials.mysql"):
+		super().__init__(app, service_name)
+
+	def create_provider(self, provider_id, config_section_name):
+		return MySQLCredentialsProvider(self.App, provider_id, config_section_name)
+
+
+class MySQLCredentialsProvider(EditableCredentialsProviderABC):
+
+	Type = "mysql"
+
+	ConfigDefaults = {
+		"host": "localhost",
+		"database": "credentials",
+		"table": "UzivatelRegistrovany",
+		"user": "root",
+		"password": "",
+		"editable": "no",
+		"field_username": "usename",
+		"field_email": "email",
+		"field_phone": "phone",
+		"field_password": "password",
+		"data_fields": ""
+	}
+
+
+	def __init__(self, app, provider_id, config_section_name):
+		super().__init__(provider_id, config_section_name)
+		self.ConnectionParams = {
+			"host": self.Config.get("host"),
+			"database": self.Config.get("database"),
+			"user": self.Config.get("user"),
+		}
+		password = self.Config.get("password")
+		if len(password) > 0:
+			self.ConnectionParams["passwd"] = password
+
+		self.Table = self.Config.get("table")
+		self.Editable = self.Config.getboolean("editable")
+
+		self.Fields = {
+			"username": self.Config.get("field_username"),
+			"email": self.Config.get("field_email"),
+			"phone": self.Config.get("field_phone"),
+			"password": self.Config.get("field_password"),
+		}
+
+		data_fields = self.Config.get("data_fields")
+		if len(data_fields) > 0:
+			self.DataFields = data_fields.split(" ")
+		else:
+			self.DataFields = None
+
+
+	async def create(self, credentials: dict) -> Optional[str]:
+		raise NotImplementedError()
+
+
+	async def register(self, register_info: dict) -> Optional[str]:
+		raise NotImplementedError()
+
+
+	async def update(self, credentials_id, update: dict) -> Optional[str]:
+		raise NotImplementedError()
+
+
+	async def delete(self, credentials_id) -> Optional[str]:
+		raise NotImplementedError()
+
+
+	async def locate(self, ident: str, ident_fields: dict = None) -> Optional[str]:
+		raise NotImplementedError()
+
+
+	async def get_by(self, key: str, value) -> Optional[dict]:
+		raise NotImplementedError()
+
+
+	async def get_by_external_login_sub(self, login_provider: str, sub_id: str) -> Optional[dict]:
+		raise NotImplementedError()
+
+
+	async def get(self, credentials_id, include=None) -> Optional[dict]:
+		raise NotImplementedError()
+
+
+	async def count(self, filtr=None) -> int:
+		raise NotImplementedError()
+
+
+	async def search(self, filter: dict = None, sort: dict = None, page: int = 0, limit: int = 0) -> list:
+		raise NotImplementedError()
+
+
+	async def iterate(self, offset: int = 0, limit: int = -1, filtr: str = None):
+		raise NotImplementedError()
+
+
+	def _nomalize_credentials(self, db_obj, include=None):
+		raise NotImplementedError()
+
+
+	async def get_login_descriptors(self, credentials_id):
+		raise NotImplementedError()
+
+
+	async def authenticate(self, credentials_id: str, credentials: dict) -> bool:
+		raise NotImplementedError()
+
+
+def authn_password(dbcred, credentials):
+		raise NotImplementedError()
