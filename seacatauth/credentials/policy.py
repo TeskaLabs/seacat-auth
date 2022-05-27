69d84e147e65223478028f78c18bb319b1c83461
Artur Zadorozhny <artur.zadorozhny@teskalabs.com>
2022-05-27 12:39:46 +0200

    Credentials: ensure user has at least an email or a phone

--- seacatauth/credentials/policy.py
+++ seacatauth/credentials/policy.py
@@ -46,7 +46,7 @@ class CredentialsPolicy:
 			"email": {
 				"type": "object",
 				"default": {
-					"creation": "required",
+					"creation": "allowed",
 					"registration": "required",
 					"editable_by": "anybody",
 				},
@@ -70,10 +70,10 @@ class CredentialsPolicy:
 				},
 			},
 		},
-		"anyOf": [  # Either "email" or "phone" must be required
-			{"properties": {"email": {"creation": {"enum": ["required"]}}}},
-			{"properties": {"phone": {"creation": {"enum": ["required"]}}}},
-		],
+		# "anyOf": [  # Either "email" or "phone" must be required
+		# 	{"properties": {"email": {"creation": {"enum": ["required"]}}}},
+		# 	{"properties": {"phone": {"creation": {"enum": ["required"]}}}},
+		# ],
 	}
 
 	def __init__(self, rbac_svc, policy_file):
@@ -138,7 +138,15 @@ class CredentialsPolicy:
 					continue
 				else:
 					raise RuntimeError("Unknown policy: {}".format(policy))
-
+		# Option 1: change policy from required to allowed and add this check
+		# At least one of (phone, email) must be specified
+		if not (validated_data.get("email") or validated_data.get("phone")):
+			L.error(
+				"Cannot create credentials: Phone or email must be specified",
+				struct_data={"username": validated_data["username"],
+							 "phone": validated_data["phone"]}
+			)
+			return None
 		# Assert there are no extra fields
 		if len(credentials_data) > 0:
 			L.error(
@@ -197,5 +205,12 @@ class CredentialsPolicy:
 				L.error("Cannot update credentials: Field update not permitted", struct_data={
 					"field": field,
 				})
+				return None
+			elif not (update_data.get("email") or update_data.get("phone")):
+				L.error(
+					"Cannot create credentials: Phone or email must be specified",
+					struct_data={"username": update_data["username"],
+								 "phone": update_data["phone"]}
+				)
 				return None
 		return update_data
