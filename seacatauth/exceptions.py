fbb6d00e4853a67ef40d6fc904bf78a421d74ade
Robin Hruška <byewokko@seznam.cz>
2022-07-12 10:47:01 +0200

    Custom NotAuthorized exception

--- seacatauth/exceptions.py
+++ seacatauth/exceptions.py
@@ -1,2 +1,25 @@
 class TenantRequired(ValueError):
 	pass
+
+
+class NotAuthorized(Exception):
+	"""
+	Request for a resource that is not accessible within current session
+	"""
+	def __init__(self, message=None, *args, tenant=None, resource=None):
+		self.Tenant = tenant
+		self.Resource = resource
+
+		if message is None:
+			items = []
+			if tenant is not None:
+				items.append("tenant '{}'".format(tenant))
+			if resource is not None:
+				items.append("resource '{}'".format(resource))
+			if len(items) > 0:
+				message = "Not authorized for {}".format(", ".join(items))
+
+		if message is None:
+			super().__init__(*args)
+		else:
+			super().__init__(message, *args)
