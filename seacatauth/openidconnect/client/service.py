6897515a641a0fd865d609d55ce8a4e5737b93e4
Robin Hruška <byewokko@seznam.cz>
2022-07-25 17:32:03 +0200

    OIDC client: Normalize and authorize methods

--- seacatauth/openidconnect/client/service.py
+++ seacatauth/openidconnect/client/service.py
@@ -17,6 +17,8 @@ class ClientService(asab.Service):
 	ClientIdPattern = r"[a-z][a-z0-9._-]{2,31}"
 	ClientSecretLength = 32
 
+	# TODO: Implement support for a list of authorized URIs (alternative to a single base URL)
+
 	def __init__(self, app, service_name="seacatauth.ClientService"):
 		super().__init__(app, service_name)
 		self.StorageService = app.get_service("asab.StorageService")
@@ -28,7 +30,7 @@ class ClientService(asab.Service):
 		# TODO: Auto-create clients for Seacat UI etc.
 
 
-	async def list(self, page: int = 0, limit: int = None, query_filter: dict = None):
+	async def list(self, page: int = 0, limit: int = None, query_filter: dict = None, include: list = None):
 		collection = self.StorageService.Database[self.ClientCollection]
 
 		if query_filter is None:
@@ -40,20 +42,28 @@ class ClientService(asab.Service):
 			cursor.skip(limit * page)
 			cursor.limit(limit)
 
-		resources = []
+		clients = []
 		count = await collection.count_documents(query_filter)
-		async for resource_dict in cursor:
-			resources.append(resource_dict)
+		async for data in cursor:
+			clients.append(self.normalize_client(data, include))
 
 		return {
-			"data": resources,
+			"data": clients,
 			"count": count,
 		}
 
 
-	async def get(self, client_id: str):
+	async def get(self, client_id: str, include: list = None):
 		data = await self.StorageService.get(self.ClientCollection, client_id)
-		return data
+		return self.normalize_client(data, include)
+
+
+	def normalize_client(self, data, include):
+		return {
+			k: v
+			for k, v in data
+			if not k.startswith("__") or k in include
+		}
 
 
 	async def create(
@@ -89,7 +99,7 @@ class ClientService(asab.Service):
 			await upsertor.execute()
 			L.log(asab.LOG_NOTICE, "OpenIDConnect Client created", struct_data={"client_id": client_id})
 		except asab.storage.exceptions.DuplicateError:
-			raise asab.exceptions.Conflict(key="id", value=client_id)
+			raise asab.exceptions.Conflict(key="client_id", value=client_id)
 
 		return {
 			"client_id": client_id,
@@ -129,3 +139,44 @@ class ClientService(asab.Service):
 	async def delete(self, client_id: str):
 		self.StorageService.delete(self.ClientCollection, client_id)
 		L.log(asab.LOG_NOTICE, "OpenIDConnect Client deleted", struct_data={"client_id": client_id})
+
+
+	async def authorize_client(
+		self,
+		client_id: str,
+		uri: str = None,
+		client_secret: str = None,
+		scope: list = None,
+	):
+		try:
+			client = await self.get(client_id, include=frozenset(["__cs"]))
+		except KeyError:
+			L.error("Invalid OpenIDConnect client ID", struct_data={
+				"client_id": client_id,
+			})
+			return False
+
+		if client_secret is not None and client_secret != client["__cs"]:  # TODO: Make secret check obligatory
+			L.error("Invalid secret for OpenIDConnect client", struct_data={
+				"client_id": client_id,
+				"client_secret": client_secret,
+			})
+			return False
+
+		if uri is not None and not uri.startswith(client["bu"]):  # TODO: Make URI check obligatory
+			L.error("OpenIDConnect client not authorized for URI", struct_data={
+				"client_id": client_id,
+				"uri": uri,
+			})
+			return False
+
+		if scope is not None:  # TODO: Make scope check obligatory
+			for scope_item in scope:
+				if scope_item not in client["s"]:
+					L.error("OpenIDConnect client not authorized for scope", struct_data={
+						"client_id": client_id,
+						"scope": scope_item,
+					})
+					return False
+
+		return True
