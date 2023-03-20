c0895f98ff8a90df005ddf05c0f4d66badae039d
Robin Hruška <byewokko@seznam.cz>
2023-03-20 16:15:16 +0100

    Remove cookie domain prefix

--- seacatauth/cookie/utils.py
+++ seacatauth/cookie/utils.py
@@ -8,7 +8,7 @@ L = logging.getLogger(__name__)
 #
 
 
-def set_cookie(app, response, session, cookie_domain_id=None, secure=None):
+def set_cookie(app, response, session, cookie_domain=None, secure=None):
 	"""
 	Add a Set-Cookie header to the response.
 	The cookie serves as an identifier of a Seacat Auth session and is used for authentication.
@@ -23,12 +23,10 @@ def set_cookie(app, response, session, cookie_domain_id=None, secure=None):
 		Identifier of cookie domain where the cookie should be valid. Defaults to root cookie domain.
 	"""
 	cookie_svc = app.get_service("seacatauth.CookieService")
-	cookie_domain = cookie_svc.get_cookie_domain(cookie_domain_id)
+	if cookie_domain is None:
+		cookie_domain = cookie_svc.RootCookieDomain
 	if secure is None:
 		secure = cookie_svc.CookieSecure
-
-	# TODO: Remove domain from the value. It is not needed anymore.
-	cookie_value = "{}:{}".format(cookie_domain, session.Cookie.Id)
 
 	# Set cookie max age equal to session max age
 	# TODO: Set cookie expiration to current session expiration and extend it dynamically
@@ -39,7 +37,7 @@ def set_cookie(app, response, session, cookie_domain_id=None, secure=None):
 
 	response.set_cookie(
 		cookie_svc.CookieName,
-		cookie_value,
+		session.Cookie.Id,
 		httponly=True,  # Not accessible from Javascript
 		domain=cookie_domain,
 		max_age=max_age,
@@ -47,6 +45,6 @@ def set_cookie(app, response, session, cookie_domain_id=None, secure=None):
 	)
 
 
-def delete_cookie(app, response, cookie_domain_id=None):
+def delete_cookie(app, response):
 	cookie_svc = app.get_service("seacatauth.CookieService")
 	response.del_cookie(cookie_svc.CookieName)
