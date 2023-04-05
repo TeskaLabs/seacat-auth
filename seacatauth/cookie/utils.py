f2be3b3fd7a6a6f28bc9a50495c6932db1a0cd7e
Robin Hruška <byewokko@seznam.cz>
2023-04-05 17:05:04 +0200

    Unique cookie names for client cookies, unique cookie values for each cookie session

--- seacatauth/cookie/utils.py
+++ seacatauth/cookie/utils.py
@@ -11,22 +11,16 @@ def set_cookie(app, response, session, cookie_domain=None, secure=None):
 	"""
 	Add a Set-Cookie header to the response.
 	The cookie serves as an identifier of a Seacat Auth session and is used for authentication.
-
-	:param app:
-		Seacat Auth application
-	:param response:
-		Response to set cookie to
-	:param session:
-		Seacat Auth session to get cookie value from
 	"""
 	cookie_svc = app.get_service("seacatauth.CookieService")
-	if cookie_domain in (None, ""):
-		cookie_domain = cookie_svc.RootCookieDomain
+
+	cookie_name = cookie_svc.get_cookie_name(session.OAuth2.ClientId)
+	cookie_domain = cookie_domain or cookie_svc.RootCookieDomain
 	if secure is None:
 		secure = cookie_svc.CookieSecure
 
 	response.set_cookie(
-		cookie_svc.CookieName,
+		cookie_name,
 		session.Cookie.Id,
 		httponly=True,  # Not accessible from Javascript
 		domain=cookie_domain,
