b84b4057f9cb656810d5a80ccd7496c7cf2f8347
Robin Hruška <byewokko@seznam.cz>
2023-06-05 14:52:16 +0200

    Allow impersonation of superuser credentials but never authorize the authz:superuser resource.

--- seacatauth/authn/service.py
+++ seacatauth/authn/service.py
@@ -438,10 +438,10 @@ class AuthenticationService(asab.Service):
 		target_authz = await build_credentials_authz(
 			self.TenantService, self.RoleService, target_cid, tenants=None)
 		if self.RBACService.is_superuser(target_authz):
-			L.warning("Cannot impersonate a superuser.", struct_data={
-				"impersonator_cid": impersonator_cid, "target_cid": target_cid})
-			raise exceptions.AccessDeniedError(
-				subject=impersonator_cid, resource="impersonate:{}".format(target_cid))
+			L.warning(
+				"Impersonation target is a superuser. Resource 'authz:superuser' will be excluded "
+				"from the impersonated session.",
+				struct_data={"impersonator_cid": impersonator_cid, "target_cid": target_cid})
 
 		scope = frozenset(["profile", "email", "phone"])
 		session_builders = [
@@ -450,7 +450,8 @@ class AuthenticationService(asab.Service):
 				tenant_service=self.TenantService,
 				role_service=self.RoleService,
 				credentials_id=target_cid,
-				tenants=None  # Root session is tenant-agnostic
+				tenants=None,  # Root session is tenant-agnostic
+				exclude_resources={"authz:superuser", "authz:impersonate"},
 			),
 			cookie_session_builder(),
 			await available_factors_session_builder(self, target_cid),
