c5979f17646aec29d5fe586c106b9178dc7cbd77
Robin Hruška <byewokko@seznam.cz>
2023-06-05 14:52:16 +0200

    Allow impersonation of superuser credentials but never authorize the authz:superuser resource.

--- seacatauth/authz/utils.py
+++ seacatauth/authz/utils.py
@@ -1,4 +1,7 @@
-async def build_credentials_authz(tenant_service, role_service, credentials_id, tenants=None):
+async def build_credentials_authz(
+	tenant_service, role_service, credentials_id,
+	tenants=None, exclude_resources=frozenset()
+):
 	"""
 	Creates a nested 'authz' dict with tenant:resource structure:
 	{
@@ -7,13 +10,16 @@ async def build_credentials_authz(tenant_service, role_service, credentials_id, 
 		'tenantB': ['resourceA', 'resourceB', 'resourceE', 'resourceD'],
 	}
 	"""
-
 	# Add global roles and resources under "*"
 	authz = {}
 	tenant = "*"
 	authz[tenant] = set()
 	for role in await role_service.get_roles_by_credentials(credentials_id, [tenant]):
-		authz[tenant].update(await role_service.get_role_resources(role))
+		authz[tenant].update(
+			res
+			for res in await role_service.get_role_resources(role)
+			if res not in exclude_resources
+		)
 	authz[tenant] = list(authz[tenant])
 
 	# Add tenant-specific roles and resources
@@ -21,7 +27,11 @@ async def build_credentials_authz(tenant_service, role_service, credentials_id, 
 		for tenant in tenants:
 			authz[tenant] = set()
 			for role in await role_service.get_roles_by_credentials(credentials_id, [tenant]):
-				authz[tenant].update(await role_service.get_role_resources(role))
+				authz[tenant].update(
+					res
+					for res in await role_service.get_role_resources(role)
+					if res not in exclude_resources
+				)
 			authz[tenant] = list(authz[tenant])
 
 	return authz
