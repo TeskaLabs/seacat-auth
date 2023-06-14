ceb241a96a9e40155b12aeebf60470c7a8a34180
Robin Hruška <byewokko@seznam.cz>
2023-06-14 20:25:55 +0200

    Remove old code

--- seacatauth/batman/handler.py
+++ seacatauth/batman/handler.py
@@ -1,9 +1,5 @@
 import logging
-import base64
-import secrets
 
-
-import asab
 import aiohttp.web
 
 #
@@ -11,16 +7,6 @@ import aiohttp.web
 L = logging.getLogger(__name__)
 
 #
-
-
-asab.Config.add_defaults(
-	{
-		"batman": {
-			"enabled": "false",
-			"oidc_url": "http://localhost:8082/openidconnect",  # The base URL for OpenID Connect
-		}
-	}
-)
 
 
 class BatmanHandler(object):
@@ -39,16 +25,9 @@ class BatmanHandler(object):
 
 		web_app = app.WebContainer.WebApp
 		web_app.router.add_put("/batman/nginx", self.batman_nginx)
-		web_app.router.add_get("/batman", self.batman_oidc_authorize_callback)
 
 		# Public endpoints
 		web_app_public.router.add_put("/batman/nginx", self.batman_nginx)
-		web_app_public.router.add_get("/batman", self.batman_oidc_authorize_callback)
-
-		self.OIDC_URL = asab.Config["batman"]["oidc_url"]
-
-		# TODO: Refresh/remove expiring grants
-		self.Grants = {}
 
 
 	async def batman_nginx(self, request):
@@ -69,49 +48,3 @@ class BatmanHandler(object):
 		return aiohttp.web.HTTPOk(headers={
 			"Authorization": "Basic {}".format(session.Batman.Token.decode("ascii"))
 		})
-
-
-	async def batman_oidc_authorize_callback(self, request):
-		"""
-		Exchange OAuth authorization code for Batman cookie
-		"""
-		code = request.query.get("code")
-		if code is None:
-			L.warning("Code has not been presented")
-			return aiohttp.web.HTTPUnauthorized()
-
-		target_location = request.query.get("state")
-		if code is None:
-			L.warning("State has not been presented")
-			return aiohttp.web.HTTPUnauthorized()
-
-		# Exchange code for access token
-		token_query = {
-			"grant_type": "batman",
-			"code": code,
-		}
-		async with aiohttp.ClientSession() as session:
-			async with session.post(self.OIDC_URL + "/token", data=token_query) as resp:
-				if resp.status != 200:
-					# The authorization of the access failed
-					return aiohttp.web.HTTPUnauthorized()
-
-				batman_token = await resp.json()
-
-				username = batman_token["username"]
-				if ":" in username:
-					L.warning("Username contains ':', that will not work with BatMan")
-					return aiohttp.web.HTTPUnauthorized()
-
-				password = self.BatmanService.generate_password(batman_token["cid"])
-
-				batman_token["ba"] = base64.b64encode((
-					"{}:{}".format(username, password)
-				).encode("ascii")).decode("ascii")
-
-		bid = secrets.token_urlsafe()
-		self.Grants[bid] = batman_token
-
-		response = aiohttp.web.HTTPSeeOther(location=target_location)
-		response.set_cookie(self.BatmanService.CookieName, bid)
-		return response
--- seacatauth/batman/service.py
+++ seacatauth/batman/service.py
@@ -13,17 +13,19 @@ L = logging.getLogger(__name__)
 
 
 class BatmanService(asab.Service):
+	"""
+	Basic Auth Token MANager
+	"""
 
 	def __init__(self, app, service_name="seacatauth.BatmanService"):
 		super().__init__(app, service_name)
-
-		self.CookieName = "BatMan"
 
 		self.Integrations = []
 		key = asab.Config.get("seacatauth:batman", "password_key")
 		if key != "":
 			self.Key = key.encode("utf-8")
 		else:
+			# TODO: There should be no hardcoded encryption password
 			self.Key = b"12345678901234567890123456789012"
 
 		if "batman:elk" in asab.Config.sections():
@@ -42,9 +44,10 @@ class BatmanService(asab.Service):
 
 
 	def generate_password(self, credentials_id):
-
+		"""
+		Generate a basic auth password using credentials ID and configured batman key.
+		"""
 		digest = hashes.Hash(hashes.SHA256(), cryptography.hazmat.backends.default_backend())
-		digest.update(credentials_id.encode('utf-8'))
+		digest.update(credentials_id.encode("utf-8"))
 		digest.update(self.Key)
-
-		return base64.b85encode(digest.finalize()).decode('ascii')
+		return base64.b85encode(digest.finalize()).decode("ascii")
