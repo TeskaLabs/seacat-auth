6d47f6f304d324dd1e7de140937a86971c0c2f55
Robin Hruška <byewokko@seznam.cz>
2023-12-20 13:35:48 +0100

    modular login session

--- seacatauth/authn/login_session.py
+++ seacatauth/authn/login_session.py
@@ -1,6 +1,6 @@
+import datetime
 import json
 import secrets
-import datetime
 import typing
 
 import cryptography.hazmat.backends
@@ -13,50 +13,45 @@ import cryptography.hazmat.primitives.ciphers.modes
 from .login_descriptor import LoginDescriptor
 
 
-class LoginSession(object):
+class SeacatLogin:
 
 	ServerLoginKeyCurve = cryptography.hazmat.primitives.asymmetric.ec.SECP256R1
 
 	def __init__(
 		self,
-		id,
-		shared_key,
-		credentials_id,
-		login_descriptors,
-		remaining_login_attempts,
-		expires_at,
-		ident,
-		public_key=None,
-		data=None
+		ident: str,
+		credentials_id: str | None,
+		login_descriptors: list | None,
+		login_attempts_left: int,
+		shared_key: bytes,
+		server_public_key: cryptography.hazmat.primitives.asymmetric.ec.EllipticCurvePublicKey,
+		data: dict | None = None
 	):
-		self.Id = id
-		self.__shared_key = shared_key
-		self.CredentialsId = credentials_id
 		self.Ident = ident
+		self.CredentialsId = credentials_id
 		self.LoginDescriptors = login_descriptors
-		self.RemainingLoginAttempts = remaining_login_attempts
-		self.ExpiresAt = expires_at
-		self.PublicKey = public_key
+		self.LoginAttemptsLeft = login_attempts_left
+		self.__shared_key = shared_key
+		self.ServerPublicKey = server_public_key
 
-		# User space for storing custom data needed by the login process
+		# Custom data needed by the login process
 		self.Data = data or {}
 
-		# Login descriptor that successfully authenticated the login session
-		self.AuthenticatedVia = None
+
+	def __repr__(self):
+		return "<SeacatLogin cid={!r} ident={!r}>".format(
+			self.CredentialsId, self.Ident)
 
 
 	@classmethod
 	def build(
 		cls,
-		client_login_key,
-		credentials_id,
-		ident,
-		login_descriptors,
-		login_attempts,
-		timeout,
-		data=None
+		ident: str,
+		credentials_id: str | None,
+		login_descriptors: list | None,
+		login_attempts_left: int,
+		client_login_key
 	):
-		# Generate shared encryption key
 		server_login_key = cryptography.hazmat.primitives.asymmetric.ec.generate_private_key(
 			cls.ServerLoginKeyCurve(),
 			cryptography.hazmat.backends.default_backend()
@@ -68,33 +63,25 @@ class LoginSession(object):
 			)
 		else:
 			shared_key = None
-
-		expires_at = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(seconds=timeout)
-
 		return cls(
-			id=secrets.token_urlsafe(),
 			shared_key=shared_key,
 			credentials_id=credentials_id,
 			ident=ident,
 			login_descriptors=login_descriptors,
-			remaining_login_attempts=login_attempts,
-			expires_at=expires_at,
-			public_key=server_login_key.public_key(),
-			data=data,
+			login_attempts_left=login_attempts_left,
+			server_public_key=server_login_key.public_key(),
 		)
 
 
 	def serialize(self) -> dict:
-		db_object = {
-			"_id": self.Id,
-			"__sk": self.__shared_key,  # TODO: Revise this. Storing plaintext shared key is not secure.
-			"pk": self.PublicKey.public_bytes(
+		return {
+			"__sk": self.__shared_key,
+			"pk": self.ServerPublicKey.public_bytes(
 				encoding=cryptography.hazmat.primitives.serialization.Encoding.PEM,
 				format=cryptography.hazmat.primitives.serialization.PublicFormat.SubjectPublicKeyInfo
 			),
 			"cid": self.CredentialsId,
-			"exp": self.ExpiresAt,
-			"la": self.RemainingLoginAttempts,
+			"la": self.LoginAttemptsLeft,
 			"ld": [
 				descriptor.serialize()
 				for descriptor in self.LoginDescriptors
@@ -102,13 +89,11 @@ class LoginSession(object):
 			"idt": self.Ident,
 			"d": self.Data,
 		}
-		return db_object
 
 
 	@classmethod
 	def deserialize(cls, authn_svc, db_object: dict):
 		return cls(
-			id=db_object["_id"],
 			shared_key=db_object["__sk"],
 			credentials_id=db_object["cid"],
 			ident=db_object["idt"],
@@ -116,9 +101,8 @@ class LoginSession(object):
 				LoginDescriptor.deserialize(authn_svc, descriptor)
 				for descriptor in db_object["ld"]
 			],
-			remaining_login_attempts=db_object["la"],
-			expires_at=db_object["exp"],
-			public_key=cryptography.hazmat.primitives.serialization.load_pem_public_key(db_object["pk"]),
+			login_attempts_left=db_object["la"],
+			server_public_key=cryptography.hazmat.primitives.serialization.load_pem_public_key(db_object["pk"]),
 			data=db_object["d"],
 		)
 
@@ -159,3 +143,122 @@ class LoginSession(object):
 
 		ciphertext = iv + encryptor.update(plaintext) + encryptor.finalize() + encryptor.tag
 		return ciphertext
+
+
+class ExternalLogin:
+	def __init__(
+		self,
+		provider_type: str,
+		nonce: str,
+	):
+		self.ProviderType = provider_type
+		self.Nonce = nonce
+
+	def __repr__(self):
+		return "<ExternalLogin provider_type={!r}>".format(
+			self.ProviderType)
+
+	def serialize(self) -> dict:
+		return {
+			"ep": self.ProviderType,
+			"en": self.Nonce,
+		}
+
+	@classmethod
+	def deserialize(cls, db_object: dict):
+		return cls(
+			provider_type=db_object["ep"],
+			nonce=db_object["en"],
+		)
+
+
+class LoginSession(object):
+
+	EncryptedFields = {"__sk"}
+
+	def __init__(
+		self,
+		id: str | None = None,
+		version: int | None = None,
+		created: datetime.datetime | None = None,
+		modified: datetime.datetime | None = None,
+		initiator_cid: str | None = None,
+		initiator_sid: str | None = None,
+		authorization_params: dict | None = None,
+		seacat_login: SeacatLogin | None = None,
+		external_login: ExternalLogin | None = None,
+	):
+		if id is None:
+			self.Id = secrets.token_urlsafe()
+		else:
+			self.Id = id
+		self.Version = version
+		self.Created = created
+		self.Modified = modified
+		self.InitiatorCredentialsId = initiator_cid
+		self.InitiatorSessionId = initiator_sid
+		self.AuthorizationParams = authorization_params
+		self.SeacatLogin = seacat_login
+		self.ExternalLogin = external_login
+
+
+	def __repr__(self):
+		return "<LoginSession {!r} seacat_login={!r} external_login={!r}>".format(
+			self.Id, self.SeacatLogin, self.ExternalLogin)
+
+
+	def serialize(self) -> dict:
+		db_object = {}
+		if self.InitiatorCredentialsId:
+			db_object["cid"] = self.InitiatorCredentialsId
+		if self.InitiatorSessionId:
+			db_object["sid"] = self.InitiatorSessionId
+		if self.AuthorizationParams:
+			db_object["ap"] = self.AuthorizationParams
+		if self.SeacatLogin:
+			db_object.update(self.SeacatLogin.serialize())
+		if self.ExternalLogin:
+			db_object.update(self.ExternalLogin.serialize())
+		return db_object
+
+
+	@classmethod
+	def deserialize(cls, authn_svc, db_object: dict):
+		return cls(
+			id=db_object["_id"],
+			version=db_object["_v"],
+			created=db_object["_c"],
+			modified=db_object["_m"],
+			initiator_cid=db_object.get("icid"),
+			initiator_sid=db_object.get("isid"),
+			authorization_params=db_object.get("ap"),
+			seacat_login=SeacatLogin.deserialize(authn_svc, db_object),
+			external_login=ExternalLogin.deserialize(db_object),
+		)
+
+
+	def initialize_seacat_login(
+		self,
+		ident: str,
+		credentials_id: str | None,
+		login_descriptors: list | None,
+		login_attempts_left: int,
+		client_login_key
+	):
+		self.SeacatLogin = SeacatLogin.build(
+			ident=ident,
+			credentials_id=credentials_id,
+			login_descriptors=login_descriptors,
+			login_attempts_left=login_attempts_left,
+			client_login_key=client_login_key
+		)
+
+
+	def decrypt(self, ciphertext: bytes) -> dict:
+		assert self.SeacatLogin is not None
+		return self.SeacatLogin.decrypt(ciphertext)
+
+
+	def encrypt(self, plaintext: typing.Union[str, dict, bytes]) -> bytes:
+		assert self.SeacatLogin is not None
+		return self.SeacatLogin.encrypt(plaintext)
