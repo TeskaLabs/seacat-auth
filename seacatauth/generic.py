d079994260d7ba9ee8a42caac8b6aee35aeecda5
Robin Hruška <byewokko@seznam.cz>
2024-02-14 12:55:08 +0100

    SearchParams utility class

--- seacatauth/generic.py
+++ seacatauth/generic.py
@@ -1,6 +1,7 @@
 import random
 import logging
 import re
+import typing
 import urllib.parse
 import aiohttp.web
 import asab
@@ -12,6 +13,58 @@ from .session import SessionAdapter
 L = logging.getLogger(__name__)
 
 #
+
+
+class SearchParams:
+	"""
+	Parse and validate standard search parameters from request query
+	"""
+	def __init__(
+		self, query: typing.Mapping, *,
+		page_default=1,
+		items_per_page_default=10,
+		simple_filter_default=None
+	):
+		# Set defaults
+		self.Page: int | None = page_default
+		self.ItemsPerPage: int | None = items_per_page_default
+		self.SimpleFilter: str | None = simple_filter_default
+		self.AdvancedFilter: dict = {}
+		self.SortBy: list = []
+
+		# Load actual parameter values from the query dict
+		for k, v in query.items():
+			if k == "p":
+				try:
+					v = int(v) - 1
+					assert v >= 1
+				except (ValueError, AssertionError) as e:
+					raise asab.exceptions.ValidationError(
+						"The value of `p` (page) query parameter must be a positive integer, not {!r}".format(v)
+					) from e
+				self.Page = v
+
+			elif k in {"i", "l"}:
+				try:
+					k = int(k)
+					assert k >= 1
+				except (ValueError, AssertionError) as e:
+					raise asab.exceptions.ValidationError(
+						"The value of `i` or `l` (items per page) query parameter must be a positive integer, "
+						"not {!r}".format(v)
+					) from e
+				self.ItemsPerPage = k
+
+			elif k == "f":
+				self.SimpleFilter = v
+
+			elif k.startswith("a"):
+				self.AdvancedFilter[k[1:]] = v
+
+			elif k.startswith("s") and v in {"a", "d"}:
+				self.SortBy.append((k[1:], v))
+
+			# Ignore any other parameter
 
 
 def get_bearer_token_value(request):
