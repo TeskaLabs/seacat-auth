0323de9a99dbcb9c3157d821b62ab5e0d68a5dce
Robin Hruška <byewokko@seznam.cz>
2024-05-23 19:37:44 +0200

    string to enum

--- seacatauth/external_login/utils.py
+++ seacatauth/external_login/utils.py
@@ -5,3 +5,14 @@ class AuthOperation(enum.StrEnum):
 	AddAccount = "a"
 	LogIn = "l"
 	SignUp = "s"
+
+	@classmethod
+	def deserialize(cls, value: str):
+		if value == "a":
+			return cls.AddAccount
+		elif value == "l":
+			return cls.LogIn
+		elif value == "s":
+			return cls.SignUp
+		else:
+			raise KeyError(value)
