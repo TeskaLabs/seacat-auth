ada5700276e3d56fc4154aee8b1aa4657de51370
Robin Hruška <byewokko@seznam.cz>
2024-05-31 12:37:09 +0200

    flake8

--- seacatauth/external_login/__init__.py
+++ seacatauth/external_login/__init__.py
@@ -3,3 +3,11 @@ from .handler.account import ExternalLoginAccountHandler
 from .handler.admin import ExternalLoginAdminHandler
 from .handler.public import ExternalLoginPublicHandler
 from . import utils
+
+__all__ = [
+	"ExternalLoginService",
+	"ExternalLoginAccountHandler",
+	"ExternalLoginAdminHandler",
+	"ExternalLoginPublicHandler",
+	"utils",
+]
