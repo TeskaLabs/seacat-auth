60cf0a205c70546b100eff14d4befc44e9e29012
Robin Hruška <byewokko@seznam.cz>
2024-06-24 17:31:17 +0200

    views init

--- seacatauth/authz/role/view/__init__.py
+++ seacatauth/authz/role/view/__init__.py
@@ -1,3 +1,9 @@
 from .global_role import GlobalRoleView
 from .shared_role import SharedRoleView
 from .tenant_role import TenantRoleView
+
+__all__ = [
+	"GlobalRoleView",
+	"SharedRoleView",
+	"TenantRoleView",
+]
