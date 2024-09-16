aaecff93084f3593504a568bff0be1e927893b4a
Robin Hruška <byewokko@seznam.cz>
2024-09-16 10:32:41 +0200

    LDAP refactoring WIP

--- seacatauth/credentials/providers/ldap.py
+++ seacatauth/credentials/providers/ldap.py
@@ -2,6 +2,7 @@ import logging
 import base64
 import datetime
 import contextlib
+import typing
 
 from typing import Optional
 
@@ -28,10 +29,6 @@ _TLS_VERSION = {
 	"1.2": ldap.OPT_X_TLS_PROTOCOL_TLS1_2,
 	"1.3": ldap.OPT_X_TLS_PROTOCOL_TLS1_3,
 }
-
-
-class LDAPObject(ldap.ldapobject.LDAPObject, ldap.resiter.ResultProcessor):
-	pass
 
 
 class LDAPCredentialsService(asab.Service):
@@ -85,21 +82,51 @@ class LDAPCredentialsProvider(CredentialsProviderABC):
 		# synchronous library (python-ldap) to be used from asynchronous code
 		self.ProactorService = proactor_svc
 
-		attr = set(self.Config["attributes"].split(" "))
-		attr.add("createTimestamp")
-		attr.add("modifyTimestamp")
-		attr.add("cn")
-		attr.add(self.Config["attrusername"])
-		self.AttrList = list(attr)
+		self.LdapUri = self.Config["uri"]
+		self.Base = self.Config["base"]
+		self.AttrList = _prepare_attributes(self.Config)
 
 		# Fields to filter by when locating a user
-		self._locate_filter_fields = ["cn", "mail", "mobile"]
+		self.IdentFields = ["mail", "mobile"]
 		# If attrusername field is not empty, locate by it too
 		if len(self.Config["attrusername"]) > 0:
-			self._locate_filter_fields.append(self.Config["attrusername"])
+			self.IdentFields.append(self.Config["attrusername"])
+
+
+	async def get(self, credentials_id, include=None) -> Optional[dict]:
+		if not credentials_id.startswith(self.Prefix):
+			raise KeyError("Credentials {!r} not found".format(credentials_id))
+		return await self.ProactorService.execute(self._get_worker, credentials_id, include)
+
+
+	async def search(self, filter: dict = None, **kwargs) -> list:
+		# TODO: Implement pagination
+		filterstr = self._build_search_filter(filter)
+		return await self.ProactorService.execute(self._search_worker, filterstr)
+
+
+	async def count(self, filtr=None) -> int:
+		filterstr = self._build_search_filter(filtr)
+		return await self.ProactorService.execute(self._count_worker, filterstr)
+
+
+	async def iterate(self, offset: int = 0, limit: int = -1, filtr: str = None):
+		filterstr = self._build_search_filter(filtr)
+		results = await self.ProactorService.execute(self._search_worker, filterstr)
+		for i in results[offset : (None if limit == -1 else limit + offset)]:
+			yield i
+
+
+	async def locate(self, ident: str, ident_fields: dict = None, login_dict: dict = None) -> str:
+		return await self.ProactorService.execute(self._locate_worker, ident, ident_fields)
+
+
+	async def authenticate(self, credentials_id: str, credentials: dict) -> bool:
+		return await self.ProactorService.execute(self._authenticate_worker, credentials_id, credentials)
 
 
 	async def get_login_descriptors(self, credentials_id):
+		# Only login with password is supported
 		return [{
 			"id": "default",
 			"label": "Use recommended login.",
@@ -110,17 +137,199 @@ class LDAPCredentialsProvider(CredentialsProviderABC):
 		}]
 
 
-	@contextlib.contextmanager
-	def _ldap_client(self):
-		ldap_client = LDAPObject(self.Config["uri"])
+	def _get_worker(self, credentials_id, include=None) -> Optional[dict]:
+		cn = base64.urlsafe_b64decode(credentials_id[len(self.Prefix):]).decode("utf-8")
+		with self._ldap_client() as lc:
+			try:
+				results = lc.search_s(
+					cn,
+					ldap.SCOPE_BASE,
+					filterstr=self.Config["filter"],
+					attrlist=self.AttrList,
+				)
+			except ldap.NO_SUCH_OBJECT as e:
+				raise KeyError("Credentials {!r} not found".format(credentials_id)) from e
+
+		if len(results) > 1:
+			L.exception("Multiple credentials matched ID.", struct_data={"cid": credentials_id})
+			raise KeyError("Credentials {!r} not found".format(credentials_id))
+
+		dn, entry = results[0]
+		return self._normalize_credentials(dn, entry)
+
+
+	def _search_worker(self, filterstr):
+		# TODO: sorting
+		result = []
+
+		with self._ldap_client() as ldap_client:
+			msgid = ldap_client.search(
+				self.Base,
+				ldap.SCOPE_SUBTREE,
+				filterstr=filterstr,
+				attrlist=self.AttrList,
+			)
+			result_iter = ldap_client.allresults(msgid)
+
+			for res_type, res_data, res_msgid, res_controls in result_iter:
+				for dn, entry in res_data:
+					if dn is not None:
+						result.append(self._normalize_credentials(dn, entry))
+
+		return result
+
+
+	def _count_worker(self, filterstr):
+		count = 0
+		with self._ldap_client() as ldap_client:
+			msgid = ldap_client.search(
+				self.Config["base"],
+				ldap.SCOPE_SUBTREE,
+				filterstr=filterstr,
+				attrsonly=1,  # If attrsonly is non-zero
+				attrlist=["cn", "mail", "mobile"],  # For counting, we need only absolutely minimum set of attributes
+			)
+			result_iter = ldap_client.allresults(msgid)
+
+			for res_type, res_data, res_msgid, res_controls in result_iter:
+				for dn, entry in res_data:
+					if dn is None:
+						continue
+					else:
+						count += 1
+
+		return count
+
+
+	def _locate_worker(self, ident: str, ident_fields: typing.Optional[typing.Mapping[str, str]]):
+		# TODO: Implement configurable ident_fields support
+		with self._ldap_client() as ldap_client:
+			msgid = ldap_client.search(
+				self.Config["base"],
+				ldap.SCOPE_SUBTREE,
+				filterstr=ldap.filter.filter_format(
+					# Build the filter template
+					# Example: (|(cn=%s)(mail=%s)(mobile=%s)(sAMAccountName=%s))
+					filter_template="(|{})".format(
+						"".join("({}=%s)".format(field) for field in self.IdentFields)),
+					assertion_values=tuple(ident for _ in self.IdentFields)
+				),
+				attrlist=["cn"],
+			)
+			result_iter = ldap_client.allresults(msgid)
+			for res_type, res_data, res_msgid, res_controls in result_iter:
+				for dn, entry in res_data:
+					if dn is not None:
+						return self._format_credentials_id(dn)
+
+		return None
+
+
+	def _authenticate_worker(self, credentials_id: str, credentials: dict) -> bool:
+		password = credentials.get("password")
+		dn = base64.urlsafe_b64decode(credentials_id[len(self.Prefix):]).decode("utf-8")
+
+		ldap_client = _LDAPObject(self.LdapUri)
 		ldap_client.protocol_version = ldap.VERSION3
 		ldap_client.set_option(ldap.OPT_REFERRALS, 0)
 
-		network_timeout = int(self.Config.get("network_timeout"))
+		# Enable TLS
+		if self.LdapUri.startswith("ldaps"):
+			self._enable_tls(ldap_client)
+
+		try:
+			ldap_client.simple_bind_s(dn, password)
+		except ldap.INVALID_CREDENTIALS:
+			L.log(asab.LOG_NOTICE, "Authentication failed: Invalid LDAP credentials.", struct_data={
+				"cid": credentials_id, "dn": dn})
+			return False
+
+		ldap_client.unbind_s()
+
+		return True
+
+
+	def _normalize_credentials(self, dn: str, search_result: typing.Mapping):
+		ret = {
+			"_id": self._format_credentials_id(dn),
+			"_type": self.Type,
+			"_provider_id": self.ProviderID,
+		}
+
+		decoded_result = {"dn": dn}
+		for k, v in search_result.items():
+			if k =="userPassword":
+				continue
+			if isinstance(v, list):
+				if len(v) == 0:
+					continue
+				elif len(v) == 1:
+					decoded_result[k] = v[0].decode("utf-8")
+				else:
+					decoded_result[k] = [i.decode("utf-8") for i in v]
+
+		v = decoded_result.pop(self.Config["attrusername"], None)
+		if v is not None:
+			ret["username"] = v
+		else:
+			# This is fallback, since we need a username on various places
+			ret["username"] = dn
+
+		v = decoded_result.pop("cn", None)
+		if v is not None:
+			ret["full_name"] = v
+
+		v = decoded_result.pop("mail", None)
+		if v is not None:
+			ret["email"] = v
+
+		v = decoded_result.pop("mobile", None)
+		if v is not None:
+			ret["phone"] = v
+
+		v = decoded_result.pop("userAccountControl", None)
+		if v is not None:
+			# userAccountControl is an array of binary flags returned as a decimal integer
+			# byte #1 is ACCOUNTDISABLE which corresponds to "suspended" status
+			# https://learn.microsoft.com/en-us/troubleshoot/windows-server/identity/useraccountcontrol-manipulate-account-properties
+			try:
+				ret["suspended"] = int(v) & 2 == 2
+			except ValueError:
+				pass
+
+		v = decoded_result.pop("createTimestamp", None)
+		if v is not None:
+			ret["_c"] = _parse_timestamp(v)
+		else:
+			v = decoded_result.pop("createTimeStamp", None)
+			if v is not None:
+				ret["_c"] = _parse_timestamp(v)
+
+		v = decoded_result.pop("modifyTimestamp", None)
+		if v is not None:
+			ret["_m"] = _parse_timestamp(v)
+		else:
+			v = decoded_result.pop("modifyTimeStamp", None)
+			if v is not None:
+				ret["_m"] = _parse_timestamp(v)
+
+		if len(decoded_result) > 0:
+			ret["_ldap"] = decoded_result
+
+		return ret
+
+
+	@contextlib.contextmanager
+	def _ldap_client(self):
+		ldap_client = _LDAPObject(self.LdapUri)
+		ldap_client.protocol_version = ldap.VERSION3
+		ldap_client.set_option(ldap.OPT_REFERRALS, 0)
+
+		network_timeout = self.Config.getint("network_timeout")
 		ldap_client.set_option(ldap.OPT_NETWORK_TIMEOUT, network_timeout)
 
 		# Enable TLS
-		if self.Config["uri"].startswith("ldaps"):
+		if self.LdapUri.startswith("ldaps"):
 			self._enable_tls(ldap_client)
 
 		ldap_client.simple_bind_s(self.Config["username"], self.Config["password"])
@@ -130,6 +339,7 @@ class LDAPCredentialsProvider(CredentialsProviderABC):
 
 		finally:
 			ldap_client.unbind_s()
+
 
 	def _enable_tls(self, ldap_client):
 		tls_cafile = self.Config["tls_cafile"]
@@ -172,119 +382,12 @@ class LDAPCredentialsProvider(CredentialsProviderABC):
 		# NEWCTX needs to be the last option, because it applies all the prepared options to the new context
 		ldap_client.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
 
-	def _get_worker(self, prefix, credentials_id, include=None) -> Optional[dict]:
 
-		# TODO: Validate credetials_id with regex
-
-		cn = base64.urlsafe_b64decode(credentials_id[len(prefix):]).decode("utf-8")
-		with self._ldap_client() as lc:
-			try:
-				sr = lc.search_s(
-					cn,
-					ldap.SCOPE_BASE,
-					filterstr=self.Config["filter"],
-					attrlist=self.AttrList,
-				)
-			except ldap.NO_SUCH_OBJECT as e:
-				L.error(e)
-				sr = []
-
-		if len(sr) == 0:
-			raise KeyError("Credentials {!r} not found".format(credentials_id))
-
-		assert len(sr) == 1
-		dn, entry = sr[0]
-
-		return _normalize_entry(
-			prefix,
-			self.Type,
-			self.ProviderID,
-			dn,
-			entry,
-			self.Config["attrusername"]
-		)
+	def _format_credentials_id(self, dn):
+		return self.Prefix + base64.urlsafe_b64encode(dn.encode("utf-8")).decode("ascii")
 
 
-	async def get(self, credentials_id, include=None) -> Optional[dict]:
-		prefix = "{}:{}:".format(self.Type, self.ProviderID)
-		if not credentials_id.startswith(prefix):
-			raise KeyError("Credentials {!r} not found".format(credentials_id))
-
-		return await self.ProactorService.execute(self._get_worker, prefix, credentials_id, include)
-
-
-	def _count_worker(self, filterstr):
-		count = 0
-		with self._ldap_client() as lc:
-			msgid = lc.search(
-				self.Config["base"],
-				ldap.SCOPE_SUBTREE,
-				filterstr=filterstr,
-				attrsonly=1,  # If attrsonly is non-zero
-				attrlist=["cn", "mail", "mobile"],  # For counting, we need only absolutely minimum set of attributes
-			)
-			result_iter = lc.allresults(msgid)
-
-			for res_type, res_data, res_msgid, res_controls in result_iter:
-				for dn, entry in res_data:
-					if dn is None:
-						continue
-					else:
-						count += 1
-
-		return count
-
-
-	async def count(self, filtr=None) -> int:
-		filterstr = self._build_search_filter(filtr)
-		return await self.ProactorService.execute(self._count_worker, filterstr)
-
-
-	def _search_worker(self, filterstr):
-
-		# TODO: sorting
-		prefix = "{}:{}:".format(self.Type, self.ProviderID)
-		result = []
-
-		with self._ldap_client() as lc:
-			msgid = lc.search(
-				self.Config["base"],
-				ldap.SCOPE_SUBTREE,
-				filterstr=filterstr,
-				attrlist=self.AttrList,
-			)
-			result_iter = lc.allresults(msgid)
-
-			for res_type, res_data, res_msgid, res_controls in result_iter:
-				for dn, entry in res_data:
-					if dn is not None:
-						result.append(_normalize_entry(
-							prefix,
-							self.Type,
-							self.ProviderID,
-							dn,
-							entry,
-							self.Config["attrusername"]
-						))
-
-		return result
-
-
-	async def search(self, filter: dict = None, **kwargs) -> list:
-		# TODO: Implement filtering and pagination
-		if filter is not None:
-			return []
-		filterstr = self.Config["filter"]
-		return await self.ProactorService.execute(self._search_worker, filterstr)
-
-
-	async def iterate(self, offset: int = 0, limit: int = -1, filtr: str = None):
-		filterstr = self._build_search_filter(filtr)
-		arr = await self.ProactorService.execute(self._search_worker, filterstr)
-		for i in arr[offset:None if limit == -1 else limit + offset]:
-			yield i
-
-	def _build_search_filter(self, filtr=None):
+	def _build_search_filter(self, filtr: typing.Optional[str] = None):
 		if not filtr:
 			filterstr = self.Config["filter"]
 		else:
@@ -299,147 +402,9 @@ class LDAPCredentialsProvider(CredentialsProviderABC):
 			)
 		return filterstr
 
-	def _locate_worker(self, ident: str):
-		with self._ldap_client() as lc:
 
-			# Build the filter template
-			# Example: (|(cn=%s)(mail=%s)(mobile=%s)(sAMAccountName=%s))
-			filter_template = "(|{})".format(
-				"".join("({}=%s)".format(field) for field in self._locate_filter_fields)
-			)
-			assertion_values = tuple(
-				ident for _ in self._locate_filter_fields
-			)
-
-			msgid = lc.search(
-				self.Config["base"],
-				ldap.SCOPE_SUBTREE,
-				filterstr=ldap.filter.filter_format(
-					filter_template=filter_template,
-					assertion_values=assertion_values
-				),
-				attrlist=["cn"]
-			)
-			result_iter = lc.allresults(msgid)
-			for res_type, res_data, res_msgid, res_controls in result_iter:
-				for dn, entry in res_data:
-					if dn is not None:
-						return "{}:{}:{}".format(
-							self.Type,
-							self.ProviderID,
-							base64.urlsafe_b64encode(dn.encode("utf-8")).decode("ascii"),
-						)
-
-		return None
-
-
-	async def locate(self, ident: str, ident_fields: dict = None, login_dict: dict = None) -> str:
-		# TODO: Implement ident_fields support
-		"""
-		Locate search for the exact match of provided ident and the username in the htpasswd file
-		"""
-		return await self.ProactorService.execute(self._locate_worker, ident)
-
-
-	def _authenticate_worker(self, credentials_id: str, credentials: dict) -> bool:
-		prefix = "{}:{}:".format(self.Type, self.ProviderID)
-
-		password = credentials.get("password")
-		dn = base64.urlsafe_b64decode(credentials_id[len(prefix):]).decode("utf-8")
-
-		lc = LDAPObject(self.Config["uri"])
-		lc.protocol_version = ldap.VERSION3
-		lc.set_option(ldap.OPT_REFERRALS, 0)
-
-		# Enable TLS
-		if self.Config["uri"].startswith("ldaps"):
-			self._enable_tls(lc)
-
-		try:
-			lc.simple_bind_s(dn, password)
-		except ldap.INVALID_CREDENTIALS:
-			L.log(asab.LOG_NOTICE, "Authentication failed: Invalid LDAP credentials.", struct_data={
-				"cid": credentials_id, "dn": dn})
-			return False
-
-		lc.unbind_s()
-
-		return True
-
-
-	async def authenticate(self, credentials_id: str, credentials: dict) -> bool:
-		return await self.ProactorService.execute(self._authenticate_worker, credentials_id, credentials)
-
-
-def _normalize_entry(prefix, ptype, provider_id, dn, entry, attrusername: str = "cn"):
-	ret = {
-		"_id": prefix + base64.urlsafe_b64encode(dn.encode("utf-8")).decode("ascii"),
-		"_type": ptype,
-		"_provider_id": provider_id,
-	}
-
-	ldap_obj = {
-		"dn": dn,
-	}
-	for k, v in entry.items():
-		if k in frozenset(["userPassword"]):
-			continue
-		if isinstance(v, list):
-			if len(v) == 1:
-				v = v[0].decode("utf-8")
-			else:
-				v = [i.decode("utf-8") for i in v]
-		ldap_obj[k] = v
-
-	v = ldap_obj.pop(attrusername, None)
-	if v is not None:
-		ret["username"] = v
-	else:
-		# This is fallback, since we need a username on various places
-		ret["username"] = dn
-
-	v = ldap_obj.pop("cn", None)
-	if v is not None:
-		ret["full_name"] = v
-
-	v = ldap_obj.pop("mail", None)
-	if v is not None:
-		ret["email"] = v
-
-	v = ldap_obj.pop("mobile", None)
-	if v is not None:
-		ret["phone"] = v
-
-	v = ldap_obj.pop("userAccountControl", None)
-	if v is not None:
-		# userAccountControl is an array of binary flags returned as a decimal integer
-		# byte #1 is ACCOUNTDISABLE which corresponds to "suspended" status
-		# https://learn.microsoft.com/en-us/troubleshoot/windows-server/identity/useraccountcontrol-manipulate-account-properties
-		try:
-			ret["suspended"] = int(v) & 2 == 2
-		except ValueError:
-			pass
-
-	v = ldap_obj.pop("createTimestamp", None)
-	if v is not None:
-		ret["_c"] = _parse_timestamp(v)
-	else:
-		v = ldap_obj.pop("createTimeStamp", None)
-		if v is not None:
-			ret["_c"] = _parse_timestamp(v)
-
-	v = ldap_obj.pop("modifyTimestamp", None)
-	if v is not None:
-		ret["_m"] = _parse_timestamp(v)
-	else:
-		v = ldap_obj.pop("modifyTimeStamp", None)
-		if v is not None:
-			ret["_m"] = _parse_timestamp(v)
-
-	if len(ldap_obj) > 0:
-		ret["_ldap"] = ldap_obj
-
-	return ret
+class _LDAPObject(ldap.ldapobject.LDAPObject, ldap.resiter.ResultProcessor):
+	pass
 
 
 def _parse_timestamp(ts: str) -> datetime.datetime:
@@ -449,3 +414,12 @@ def _parse_timestamp(ts: str) -> datetime.datetime:
 		pass
 
 	return datetime.datetime.strptime(ts, r"%Y%m%d%H%M%S.%fZ")
+
+
+def _prepare_attributes(config: typing.Mapping):
+	attr = set(config["attributes"].split(" "))
+	attr.add("createTimestamp")
+	attr.add("modifyTimestamp")
+	attr.add("cn")
+	attr.add(config["attrusername"])
+	return list(attr)
