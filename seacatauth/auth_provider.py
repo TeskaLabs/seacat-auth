8b8d25fc25c6d53f774feb28f3271e8598c1fc8e
Robin Hruška <byewokko@seznam.cz>
2025-02-13 15:56:15 +0100

    system_authz context

--- seacatauth/auth_provider.py
+++ seacatauth/auth_provider.py
@@ -1,5 +1,12 @@
+import contextlib
+import datetime
 import logging
+import typing
+import secrets
 import aiohttp.web
+import jwcrypto.jwt
+import jwcrypto.jwk
+import jwcrypto.jws
 import asab
 import asab.exceptions
 import asab.web.auth
@@ -29,9 +36,11 @@ class AsabAuthProvider(asab.web.auth.providers.IdTokenAuthProvider):
 	def __init__(self, app):
 		super().__init__(app)
 		self.SessionService = app.get_service("seacatauth.SessionService")
+
+		# Add Seacat Auth application key
 		key_provider = asab.web.auth.providers.key_providers.StaticPublicKeyProvider(app)
-		key_provider.set_public_key(app.PrivateKey.public())
 		self.register_key_provider(key_provider)
+		key_provider.set_public_key(app.PrivateKey.public())
 
 
 	async def authorize(self, request: aiohttp.web.Request) -> Authorization:
@@ -74,3 +83,56 @@ class AsabAuthProvider(asab.web.auth.providers.IdTokenAuthProvider):
 
 		self.Authorizations[id_token] = authz
 		return authz
+
+
+	async def _get_claims_from_id_token(self, id_token):
+		"""
+		Parse the bearer ID token and extract auth claims.
+		"""
+		try:
+			return asab.web.auth.utils.get_id_token_claims(id_token, self.TrustedJwkSet)
+		except (jwcrypto.jws.InvalidJWSSignature, jwcrypto.jwt.JWTMissingKey) as e:
+			L.error("Cannot authenticate request: {}".format(str(e)))
+			raise asab.exceptions.NotAuthenticatedError()
+
+
+@contextlib.contextmanager
+def system_authz(
+	issuer: str,
+	resources: typing.Collection[str],
+	tenant: str | None,
+	expiration: int = 60
+) -> Authorization:
+	authorized_resources = {
+		tenant: set(resources),
+	}
+	session_id = secrets.token_urlsafe(16)
+	now = datetime.datetime.now(datetime.UTC)
+	session_dict = {
+		SessionAdapter.FN.SessionId: session_id,
+		SessionAdapter.FN.Version: 0,
+		SessionAdapter.FN.CreatedAt: now,
+		SessionAdapter.FN.ModifiedAt: None,
+		SessionAdapter.FN.Session.Type: "SYSTEM",
+		SessionAdapter.FN.Session.Expiration: now + datetime.timedelta(seconds=expiration),
+		SessionAdapter.FN.Authorization.Authz: authorized_resources,
+		SessionAdapter.FN.Credentials.Id: "SYSTEM",
+	}
+	session = SessionAdapter(session_dict)
+
+	claims = {
+		"resources": authorized_resources,
+		"sid": session_id,
+		"iss": issuer,
+		"iat": now.timestamp(),
+		"exp": now.timestamp() + expiration,
+	}
+	authz = Authorization(claims, session)
+
+	authz_ctx = asab.contextvars.Authz.set(authz)
+	try:
+		yield authz
+	finally:
+		asab.contextvars.Authz.reset(authz_ctx)
+		del authz
+		del session
