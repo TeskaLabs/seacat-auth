5becb1126189b923916a22afcc90acb603a8bd06
Robin Hruška <byewokko@seznam.cz>
2025-06-02 18:27:30 +0200

    make global_only a true resource attribute stored in the database

--- seacatauth/authz/resource/schema.py
+++ seacatauth/authz/resource/schema.py
@@ -2,7 +2,15 @@ CREATE_OR_UNDELETE_RESOURCE = {
 	"type": "object",
 	"additionalProperties": False,
 	"properties": {
-		"description": {"type": "string"}}
+		"description": {"type": "string"},
+		"global_only": {
+			"type": "boolean",
+			"description":
+				"If set to true, the resource has a global scope and cannot be granted via tenant-specific roles. "
+				"This attribute cannot be changed later.",
+			"default": False,
+		},
+	}
 }
 
 UPDATE_RESOURCE = {
