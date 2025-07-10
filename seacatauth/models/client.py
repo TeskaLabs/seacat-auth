1e4479281d84afcc71f3d135d151fb5cd513eb53
Robin Hruška <byewokko@seznam.cz>
2025-07-10 14:25:56 +0200

    separate oauth client config

--- seacatauth/models/client.py
+++ seacatauth/models/client.py
@@ -2,53 +2,18 @@ import logging
 import datetime
 import typing
 
+from .. import generic
 from ..models.const import OAuth2
 
 
 L = logging.getLogger(__name__)
 
 
-class Client:
+class _OAuth2Config:
+	"""
+	OAuth 2.0 / OpenID Connect client configuration
+	"""
 
-	def __init__(self, client_dict: dict):
-		self._Dict: dict = client_dict
-		self.Id: str = self._Dict["_id"]
-		self.Version: int = self._Dict["_v"]
-		self.CreatedAt: datetime.datetime = self._Dict["_c"]
-		self.ModifiedAt: datetime.datetime = self._Dict["_m"]
-
-		self.Label: str = self._Dict.get("client_name", self.Id)
-		self.SeacatAuthCredentialsEnabled: bool = self._Dict["seacatauth_credentials"]
-		self.SeacatAuthCredentialsId: typing.Optional[str] = self._Dict["seacatauth_credentials_id"]
-
-		self.OAuth2: typing.Optional[_OAuth2] = _OAuth2.deserialize(self._Dict)
-
-
-	def __repr__(self):
-		return ("<Client {!r} (OAuth: {})>".format(
-			self.Id,
-			self.OAuth2 is not None,
-		))
-
-
-	def __getitem___(self, key: str):
-		return self._Dict[key]
-
-
-	def rest_get(self) -> dict:
-		client_dict = {
-			"_id": self.Id,
-			"_v": self.Version,
-			"_c": self.CreatedAt,
-			"_m": self.ModifiedAt,
-			"label": self.Label,
-		}
-		if self.OAuth2 is not None:
-			client_dict["oauth"] = self.OAuth2.rest_get()
-		return client_dict
-
-
-class _OAuth2:
 	def __init__(self, client_dict: dict):
 		# OAuth 2.0 / OpenID Connect attributes
 		self.ClientId: str = client_dict.get("client_id")
@@ -66,6 +31,7 @@ class _OAuth2:
 
 		# SeaCat Auth attributes
 		self.RedirectUriValidationMethod: str = client_dict.get("redirect_uri_validation_method")
+		self.CookieName: str = client_dict.get("cookie_name")
 		self.CookieDomain: str = client_dict.get("cookie_domain")
 		self.CookieWebhookUri: str = client_dict.get("cookie_webhook_uri")
 		self.CookieEntryUri: str = client_dict.get("cookie_entry_uri")
@@ -77,7 +43,7 @@ class _OAuth2:
 
 
 	@classmethod
-	def deserialize(cls, client_dict: dict):
+	def deserialize(cls, client_dict: dict) -> typing.Optional["_OAuth2Config"]:
 		"""
 		Deserializes a client dictionary into an _OAuth2 instance.
 		If the dictionary does not contain the mandatory OAuth2 client metadata, it returns None.
@@ -92,14 +58,26 @@ class _OAuth2:
 
 
 	def is_confidential(self) -> bool:
-		if self.TokenEndpointAuthMethod not in OAuth2.TokenEndpointAuthMethod:
-			raise NotImplementedError(
-				"Unsupported token_endpoint_auth_method: {!r}".format(self.TokenEndpointAuthMethod))
-
 		if self.TokenEndpointAuthMethod == OAuth2.TokenEndpointAuthMethod.NONE:
 			return False
 		else:
 			return True
+
+
+	def authenticate(self, client_secret: str) -> bool:
+		"""
+		Authenticates the client using the provided client secret.
+		Returns True if the authentication is successful, otherwise False.
+		"""
+		if self._ClientSecretHash is None:
+			L.error("Client has no client secret set.", struct_data={"client_id": self.ClientId})
+			return False
+
+		if self.ClientSecretExpiresAt and self.ClientSecretExpiresAt < datetime.datetime.now(datetime.timezone.utc):
+			L.error("Expired client secret.", struct_data={"client_id": self.ClientId})
+			return False
+
+		return generic.argon2_verify(self._ClientSecretHash, client_secret)
 
 
 	def rest_get(self) -> dict:
@@ -122,7 +100,7 @@ class _OAuth2:
 			"login_uri": self.LoginUri,
 			"authorize_anonymous_users": self.AuthorizeAnonymousUsers,
 			"anonymous_cid": self.AnonymousCid,
-			"session_expiration": self.SessionExpiration
+			"session_expiration": self.SessionExpiration,
 		}
 
 		if self._ClientSecretHash is not None:
@@ -131,3 +109,45 @@ class _OAuth2:
 			oauth_dict["client_secret_expires_at"] = self.ClientSecretExpiresAt
 
 		return oauth_dict
+
+
+class Client:
+
+	def __init__(self, client_dict: dict):
+		self._Dict: dict = client_dict
+		self.Id: str = self._Dict["_id"]
+		self.Version: int = self._Dict["_v"]
+		self.CreatedAt: datetime.datetime = self._Dict["_c"]
+		self.ModifiedAt: datetime.datetime = self._Dict["_m"]
+
+		self.Label: str = self._Dict.get("client_name", self.Id)
+		self.SeacatAuthCredentialsEnabled: bool = self._Dict.get("seacatauth_credentials")
+		self.SeacatAuthCredentialsId: typing.Optional[str] = self._Dict.get("seacatauth_credentials_id")
+
+		self.OAuth2: typing.Optional[_OAuth2Config] = _OAuth2Config.deserialize(self._Dict)
+
+
+	def __repr__(self):
+		return ("<Client {!r} (OAuth: {})>".format(
+			self.Id,
+			self.OAuth2 is not None,
+		))
+
+
+	def __getitem__(self, key: str):
+		return self._Dict[key]
+
+
+	def rest_get(self) -> dict:
+		client_dict = {
+			"_id": self.Id,
+			"_v": self.Version,
+			"_c": self.CreatedAt,
+			"_m": self.ModifiedAt,
+			"label": self.Label,
+			"seacatauth_credentials": self.SeacatAuthCredentialsEnabled,
+			"seacatauth_credentials_id": self.SeacatAuthCredentialsId,
+		}
+		if self.OAuth2 is not None:
+			client_dict["oauth"] = self.OAuth2.rest_get()
+		return client_dict
