599699776cb565b2844c71f5cd05d952198b60d8
Robin Hruška <11579460+byewokko@users.noreply.github.com>
2025-10-03 14:04:53 +0200

    Fix pysaml2 dependency (#519)
    * import providers conditionally to avoid unused dependencies
    
    * add xmlsec dependency
    
    * update CHANGELOG.md

--- seacatauth/external_login/authentication/__init__.py
+++ seacatauth/external_login/authentication/__init__.py
@@ -1,9 +1,7 @@
 from .service import ExternalAuthenticationService
 from .handler import ExternalAuthenticationHandler
-from . import providers
 
 __all__ = [
 	"ExternalAuthenticationService",
 	"ExternalAuthenticationHandler",
-	"providers",
 ]
--- seacatauth/external_login/authentication/providers/__init__.py
+++ seacatauth/external_login/authentication/providers/__init__.py
@@ -1,14 +1,3 @@
-from .abc import ExternalAuthProviderABC
-from .oauth2 import OAuth2AuthProvider
-from .github import GitHubOAuth2AuthProvider
-from .google import GoogleOAuth2AuthProvider
-from .office365 import Office365OAuth2AuthProvider
-from .mojeid import MojeIDOAuth2AuthProvider
-from .facebook import FacebookOAuth2AuthProvider
-from .appleid import AppleIDOAuth2AuthProvider
-from .saml import SamlAuthProvider
-
-
 def create_provider(authn_handler, section):
 	parts = section.split(":", 1)
 	if len(parts) != 2 or parts[0] != "seacatauth":
@@ -16,48 +5,53 @@ def create_provider(authn_handler, section):
 
 	provider_type = parts[1]
 	if provider_type == "oauth2:github":
+		from .github import GitHubOAuth2AuthProvider
 		return GitHubOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "oauth2:google":
+		from .google import GoogleOAuth2AuthProvider
 		return GoogleOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "oauth2:office365":
+		from .office365 import Office365OAuth2AuthProvider
 		return Office365OAuth2AuthProvider(authn_handler, section)
 	if provider_type == "oauth2:mojeid":
+		from .mojeid import MojeIDOAuth2AuthProvider
 		return MojeIDOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "oauth2:facebook":
+		from .facebook import FacebookOAuth2AuthProvider
 		return FacebookOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "oauth2:appleid":
+		from .appleid import AppleIDOAuth2AuthProvider
 		return AppleIDOAuth2AuthProvider(authn_handler, section)
 	if provider_type.startswith("oauth2:"):
+		from .oauth2 import OAuth2AuthProvider
 		return OAuth2AuthProvider(authn_handler, section)
 	if provider_type.startswith("saml:"):
+		from .saml import SamlAuthProvider
 		return SamlAuthProvider(authn_handler, section)
 
 	# Backward compatibility for old provider type names
 	if provider_type == "github":
+		from .github import GitHubOAuth2AuthProvider
 		return GitHubOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "google":
+		from .google import GoogleOAuth2AuthProvider
 		return GoogleOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "office365":
+		from .office365 import Office365OAuth2AuthProvider
 		return Office365OAuth2AuthProvider(authn_handler, section)
 	if provider_type == "mojeid":
+		from .mojeid import MojeIDOAuth2AuthProvider
 		return MojeIDOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "facebook":
+		from .facebook import FacebookOAuth2AuthProvider
 		return FacebookOAuth2AuthProvider(authn_handler, section)
 	if provider_type == "appleid":
+		from .appleid import AppleIDOAuth2AuthProvider
 		return AppleIDOAuth2AuthProvider(authn_handler, section)
 
 	return None
 
 
 __all__ = [
-	"ExternalAuthProviderABC",
-	"AppleIDOAuth2AuthProvider",
-	"FacebookOAuth2AuthProvider",
-	"GitHubOAuth2AuthProvider",
-	"GoogleOAuth2AuthProvider",
-	"MojeIDOAuth2AuthProvider",
-	"OAuth2AuthProvider",
-	"Office365OAuth2AuthProvider",
-	"SamlAuthProvider",
 	"create_provider",
 ]
