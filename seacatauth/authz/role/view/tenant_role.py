54972986185cbfdc9915103d026e3a595784b932
Robin Hruška <11579460+byewokko@users.noreply.github.com>
2025-11-25 16:00:57 +0100

    Role API redesign (#487)
    * new role assign paths
    
    * add assign_cid to role list
    
    * explicit params in SearchParams
    
    * implement role filter using aggregation pipeline (wip)
    
    * add role _public_id
    
    * debug tenant filtering
    
    * role sorting
    
    * update docstring
    
    * import BoolFieldOp
    
    * resource filter allows only one resource
    
    * remove unused method
    
    * replace deprecated operator
    
    * decorate abstract methods
    
    * move contextual role filtering to get_credentials_roles endpoint
    
    * fix typing, add docstrings
    
    * fix typos and leftovers
    
    * handle null pipeline
    
    * reverse string sort utility
    
    * fix typing
    
    * fix role normalization
    
    * fix role pagination
    
    * remove unused kwargs
    
    * prefix role API with /admin, keep old routes for back compat
    
    * proper global handlers
    
    * fix assigned filter
    
    * list_role_credentials
    
    * fix typo
    
    * role assignment as a nested object
    
    * debugging role credentials list
    
    * fix pagination
    
    * fix filtering for users with no roles
    
    * align signatures
    
    * add back resource param for backward compatibility
    
    * add missing documentation
    
    * fix _get_role_credentials
    
    * simplify, removing unused branch
    
    * update changelog

--- seacatauth/authz/role/view/tenant_role.py
+++ seacatauth/authz/role/view/tenant_role.py
@@ -1,4 +1,3 @@
-import re
 import typing
 
 from .abc import RoleView
@@ -14,54 +13,24 @@ class CustomTenantRoleView(RoleView):
 		self.TenantId = tenant_id
 
 
-	def _build_query(
-		self,
-		name_filter: typing.Optional[str] = None,
-		resource_filter: typing.Optional[str] = None,
-		**kwargs
-	):
-		query = {"tenant": self.TenantId}
-		if name_filter:
-			query["_id"] = {"$regex": re.escape(name_filter)}
-		if resource_filter:
-			query["resources"] = resource_filter
-		return query
-
-
-	async def count(
-		self,
-		name_filter: typing.Optional[str] = None,
-		resource_filter: typing.Optional[str] = None,
-		**kwargs
-	) -> int | None:
-		query = self._build_query(name_filter=name_filter, resource_filter=resource_filter)
-		return await self.StorageService.Database[self.CollectionName].count_documents(query)
-
-
-	async def iterate(
-		self,
-		offset: int = 0,
-		limit: typing.Optional[int] = None,
-		sort: typing.Tuple[str, int] = ("_id", 1),
-		name_filter: typing.Optional[str] = None,
-		resource_filter: typing.Optional[str] = None,
-		**kwargs
-	) -> typing.AsyncGenerator:
-		query = self._build_query(name_filter=name_filter, resource_filter=resource_filter)
-		async for role in self._iterate(offset, limit, query, sort):
-			yield self._normalize_role(role)
-
-
 	async def get(self, role_id: str) -> dict:
 		assert self._role_tenant_matches(role_id)
 		return self._normalize_role(await self.StorageService.get(self.CollectionName, role_id))
 
 
-	def _role_tenant_matches(self, role_id: str):
+	def _base_query(self) -> dict:
+		return {"tenant": self.TenantId}
+
+
+	def _role_tenant_matches(self, role_id: str) -> bool:
 		return role_id.split("/")[0] == self.TenantId
 
 
-	def _normalize_role(self, role: dict):
+	def _is_tenant_match(self, tenants: typing.Iterable[str]) -> bool:
+		return self.TenantId in tenants
+
+
+	def _normalize_role(self, role: dict) -> dict:
 		role = super()._normalize_role(role)
 		role["type"] = "tenant"
 		return role
