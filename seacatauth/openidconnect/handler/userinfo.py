3ad6432fb36b03765670c3a3852cdde4a24e2cf4
Robin Hruška <byewokko@seznam.cz>
2025-12-01 10:51:43 +0100

    fix scope typing

--- seacatauth/openidconnect/handler/userinfo.py
+++ seacatauth/openidconnect/handler/userinfo.py
@@ -82,7 +82,7 @@ class UserInfoHandler(object):
 					scope=["openid"],
 				)
 
-		if "openid" not in session.OAuth2.Scope.split(" "):
+		if "openid" not in session.OAuth2.Scope:
 			L.log(asab.LOG_NOTICE, "Insufficient scope.")
 			return asab.exceptions.NotAuthenticatedError(
 				error="insufficient_scope",
