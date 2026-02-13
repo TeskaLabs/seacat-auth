5e98f38cd1b4739177cd4d5e225164ea8e7b0a6d
Robin Hruška <byewokko@seznam.cz>
2026-02-13 18:10:10 +0100

    defensive approach

--- seacatauth/client/service.py
+++ seacatauth/client/service.py
@@ -807,6 +807,8 @@ def _set_credentials_id(app, client: dict) -> dict:
 
 def _set_cookie_name(app, client: dict) -> dict:
 	cookie_svc = app.get_service("seacatauth.CookieService")
+	if not cookie_svc:
+		return client
 	client["cookie_name"] = cookie_svc.get_cookie_name(client["_id"])
 	return client
 
