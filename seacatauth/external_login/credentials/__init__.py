c9d07c94006dc621e091857271680ccc48a1032f
Robin Hruška <11579460+byewokko@users.noreply.github.com>
2026-02-13 14:55:41 +0100

    Common admin API for authentication methods (#533)
    * fix admin webauthn list
    
    * consistent authn admin route
    
    * split authn handlers, introduce admin handler with authn method list draft
    
    * add provider_label for convenience
    
    * aggregate all authentication methods
    
    * authentication method api wip
    
    * public get_totp method
    
    * implement get, delete and normalize methods
    
    * present available actions
    
    * public method docstrings
    
    * unify authn method schema
    
    * introduce authn method providers, replace old admin handlers
    
    * remove unused
    
    * propagate creation ts
    
    * handle notfound errors
    
    * fix webauthn method flow
    
    * update changelog
    
    * add missing cred id
    
    * defensive None check
    
    * defensive id decoding

--- seacatauth/external_login/credentials/__init__.py
+++ seacatauth/external_login/credentials/__init__.py
@@ -1,9 +1,7 @@
 from .service import ExternalCredentialsService
-from .handler.admin import ExternalCredentialsAdminHandler
 from .handler.account import ExternalLoginAccountHandler
 
 __all__ = [
 	"ExternalCredentialsService",
 	"ExternalLoginAccountHandler",
-	"ExternalCredentialsAdminHandler",
 ]
